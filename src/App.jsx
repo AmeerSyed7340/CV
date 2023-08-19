b138a8cb4ee1a16fd926dfeec261e0ffbf7090e1
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-19 15:36:25 -0400

    Added generic component for form and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Form from './components/Form'
+import Display from './components/Display'
 function App() {
-
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
   return (
     <>
-    <Form />
+      <Form legend="General Info" inputFeilds={[
+        { type: "text", placeholder: "name", setter: setName },
+        { type: "email", placeholder: "Email", setter: setEmail },
+        { type: "number", placeholder: "Phone", setter: setPhone }]} />
+      <div>{name} {email} {phone}</div>
     </>
   )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,18 @@
-export default function Form(props){
+export default function Form({legend, inputFeilds }) {
+    function handleSubmit(event){
+        event.preventDefault();
+        inputFeilds.forEach((inputfeild, index) => {
+            inputfeild.setter(event.target[index].value)
+        })
+    }
     return (
         <>
-            <form>
-                <label htmlFor="name">Name: </label>
-                <input type="text" id="name"/>
+            <form onSubmit={handleSubmit}>
+                <legend>{legend}</legend>
+                {inputFeilds.map(field => (
+                    <input key={field.placeholder} type={field.type} placeholder={field.placeholder} />
+                ))}
+                <button type="submit">Submit</button>
             </form>
         </>
     )
