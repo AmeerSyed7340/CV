2aa014a93a7daeff12e515f52af21bd526bacce4
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-19 14:22:43 -0400

    Initial cleanup

---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+export default function Form(props){
+    return (
+        <>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input type="text" id="name"/>
+            </form>
+        </>
+    )
+}
